bf3e1e9c57a2bf93696ac6d4723e30021cf1ec1f
pachevang <vangpache@gmail.com>
2019-08-22 14:22:51 -0500

    adds to checkout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import Checkout from './components/Checkout/Checkout';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Checkout />, document.getElementById('root'));
