e44ce2541be8c94452f7eef7565b1429bf9eccf1
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-22 14:23:43 -0500

    creates and completes customer form component

---
+++ src/components/EnterCustomerInfo/EnterCustomerInfo.jsx
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import axios from 'axios';
+
+
+class EnterCustomerInfo extends Component {
+
+state = {
+    newCustomer: {
+        customer_name: '',
+        street_address: '',
+        city: '',
+        zip: '',
+        type: true,
+    }
+}
+
+handleChangeFor = (propertyName, event) => {
+    this.setState({
+        newCustomer: {
+            ...this.state.newCustomer,
+            [propertyName]:event.target.value
+        }
+    })
+}
+
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('you clicked the submit', this.state.newCustomer);
+    axios.post('/api/order', this.state.newCustomer)
+    .then(response => {
+    console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    this.props.history.push();
+}
+
+render(){ 
+
+    return (
+        
+        <div>
+            <header>
+                <h1>Prime Pizza</h1>
+                {/* <h2>Total: {this.props.reduxStore.cart.totalPrice}</h2> */}
+            </header>
+        <h1>Hello From Customer Info Form</h1>
+        <form onSubmit = {this.handleSubmit}>
+            <input type="text" placeholder = "Name"
+            onChange={(event) => this.handleChangeFor('customer_name', event)}/>
+            <input type="text" placeholder= "Your Street Address"
+            onChange={(event) => this.handleChangeFor('street_address', event)}/>
+            <input type = "text" placeholder ="Your City"
+            onChange = {(event) => this.handleChangeFor('city', event)}/>
+            <input type= "number" placeholder = "Your Zip Code"
+             onChange = {(event) => this.handleChangeFor('zip', event)} />
+                    <input type="radio" name="delivery" value="Pickup" checked={this.setState({ newCustomer: { type: false}})} 
+            className = "form-check-input" />
+                    <input type="radio" name="delivery" value="Delivery" checked={this.setState({ newCustomer: { type: true }})} 
+            className = "form-check-input" />
+                <button type="submit" >Next</button>
+        </form>
+        </div>
+       
+    )
+}
+}
+
+const mapStateToProps = (reduxStore) => {
+    return {reduxStore}
+}
+
+export default connect(mapStateToProps)(EnterCustomerInfo)
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import EnterCustomerInfo from './components/EnterCustomerInfo/EnterCustomerInfo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<EnterCustomerInfo />, document.getElementById('root'));
