4467578ef22f2f21ec356db150e118c7e511b471
Andy DuBois <andydubois89@gmail.com>
2019-08-22 16:38:08 -0500

    next button works, set up divs to display inline block

--- src/components/SelectPizza/SelectPizza.jsx
+++ src/components/SelectPizza/SelectPizza.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
-import axios from 'axios'
-import {connect} from 'react-redux'
+import axios from "axios";
+import { connect } from "react-redux";
 
 class SelectPizza extends Component {
-    state = {
-        pizzas : []
-    }
+  state = {
+    pizzas: []
+  };
 
-handleClick = (id, name, price) => {
-    this.props.dispatch({
-        type: "ADD_PIZZA",
-        payload: {id, name, price}
-    })
-}
-
-    componentDidMount() {
+  componentDidMount() {
     this.getPizzas();
   }
 
+  handleAddPizza = (id, name, price) => {
+    this.props.dispatch({
+      type: "ADD_PIZZA",
+      payload: { id, name, price }
+    });
+  };
+
+  handleDeletePizza = id => {
+    this.props.dispatch({
+      type: "REMOVE_PIZZA",
+      payload: { id }
+    });
+  };
+
+  changePage = () => {
+    this.props.history.push("/order");
+  };
+
   getPizzas = () => {
-    axios.get("/api/pizza")
+    axios
+      .get("/api/pizza")
       .then(response => {
         console.log(response.data);
         this.props.dispatch({
@@ -40,11 +52,24 @@ handleClick = (id, name, price) => {
           <p>Total: $.$$</p>
         </header>
         <div className='pizzaContainer'>
-            {this.props.reduxStore.pizzaList.map(pizza => (
-                <div className="pizzaBox" key={pizza.id}><h2>{pizza.name}</h2><h4>{pizza.description}</h4><p>${pizza.price}</p><button onClick={() => this.handleClick (pizza.id, pizza.name, pizza.price)}>Add to Order</button></div>
-            ))}
+          {this.props.reduxStore.pizzaList.map(pizza => (
+            <div className='pizzaBox' key={pizza.id}>
+              <h2>{pizza.name}</h2>
+              <h4>{pizza.description}</h4>
+              <p>${pizza.price}</p>
+              <button
+                onClick={() =>
+                  this.handleAddPizza(pizza.id, pizza.name, pizza.price)
+                }>
+                Add to Order
+              </button>
+              <button onClick={() => this.handleDeletePizza(pizza.id)}>
+                Delete
+              </button>
+            </div>
+          ))}
         </div>
-        <button>Next</button>
+        <button onClick={() => this.changePage()}>Next</button>
       </>
     );
   }
@@ -53,6 +78,5 @@ handleClick = (id, name, price) => {
 const mapStateToProps = reduxStore => ({
   reduxStore
 });
-
 
 export default connect(mapStateToProps)(SelectPizza);
