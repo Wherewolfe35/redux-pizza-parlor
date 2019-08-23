8473079b433dfee0f68ec4ccb749883dda723b9c
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-23 13:05:35 -0500

    material UI is a thinkg I do no

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { HashRouter as Router, Route, } from 'react-router-dom';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+// import 'typeface-roboto';
+
 
 //Components
 import Home from "../Home/Home";
@@ -9,17 +12,36 @@ import Checkout from "../Checkout/Checkout";
 import EnterCustomerInfo from "../EnterCustomerInfo/EnterCustomerInfo";
 import Admin from '../Admin/Admin'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      
+      main: "#ff3d00"
+    },
+    secondary: {
+      light: "#ffff56",
+      main: "#ffea00"
+    },
+    status: {
+    danger: "orange"
+  },
+    background:{ 
+      default: "#ff7043"}
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-      <Route exact path='/' component={Home}/>
-      <Route path='/select' component={SelectPizza} />
-      <Route path='/checkout' component={Checkout}/>
-      <Route path='/order' component={EnterCustomerInfo} />
-      <Route path ='/admin' component ={Admin}/>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/select" component={SelectPizza} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/order" component={EnterCustomerInfo} />
+          <Route path="/admin" component={Admin} />
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
